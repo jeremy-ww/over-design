9c4fbbd4e2a2fa2028e2fe063988f573008a3178
Jeremy Wu <jeremy.wu01@sap.com>
2021-06-10 15:25:42 +0800

    prelude

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>xxxx111</div>, document.querySelector('#root'));
+ReactDOM.render(<div>demo</div>, document.querySelector('#root'));
