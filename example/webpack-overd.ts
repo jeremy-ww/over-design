c8cc7e76a587241bc6b42036865de5264dbc8acb
Jeremy Wu <wuchengkai0@gmail.com>
2021-06-18 14:32:25 +0800

    Update webpack-overd.ts

--- example/webpack-overd.ts
+++ example/webpack-overd.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line func-names
 export default function (config: import('webpack').Configuration) {
-  // eslint-disable-next-line no-console
-  console.log(config);
+  config.output.publicPath = '/over-design/'
 }
