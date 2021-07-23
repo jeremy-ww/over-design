c2bfaccb82a6ab9f9aac6a6f847490a3435cd2aa
Jeremy Wu <wuchengkai0@gmail.com>
2021-07-23 16:36:18 +0800

    Update store.tsx

--- example/src/common/store.tsx
+++ example/src/common/store.tsx
@@ -24,3 +24,6 @@ const store = configureStore({
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
